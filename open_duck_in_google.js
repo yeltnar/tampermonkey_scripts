7545c069ff49f0109298a443661530e0f361b1ff
yeltnar <andcbrant@gmail.com>
2019-04-12 11:51:34 -0500

    Update open_duck_in_google.js

--- open_duck_in_google.js
+++ open_duck_in_google.js
@@ -1,27 +1,33 @@
 // ==UserScript==
-// @name         open duck in google
+// @name         open duck in google gg
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  open duck duck go search in google
 // @author       You
 // @match        https://duckduckgo.com/?q=*
 // @grant        window.close
+// @run-at document-start
 // ==/UserScript==
 
 let key ="";
 
+const special_key = "g";
+const number_of_prefrences = 2;
+let long_special_key;
+
+(()=>{
+    const arr = [];
+    for(let i=0; i<number_of_prefrences; i++){
+        arr.push(special_key);
+    }
+    long_special_key = arr.join("");
+})();
+
 let keyAction = (e)=>{
     key += e.key;
-    if( key==="gg" ){
-        let txt = document.querySelectorAll("#search_form_input")[0].innerText;
-
-        const href = window.location.href;
-        const split_str = "?q=";
-        const q = split_str+href.split(split_str)[1]
-        open("https://google.com/search"+q);
-
-        window.close();
-    }else if( key==="g" ){
+    if( key===long_special_key ){
+        moveToGoogle();
+    }else if( key===special_key ){
 
     }else{
         key =""
@@ -30,6 +36,40 @@ let keyAction = (e)=>{
     console.log(key);
     console.log(e.key);
 };
+
+function moveToGoogle(){
+
+    let search = window.location.search;
+
+    search = search.split("?")[1];
+
+    let search_arr = search.split("&");
+
+    let q = search_arr.reduce((acc, cur)=>{
+
+        if( acc!==undefined ){
+            return acc;
+        }
+
+        const tmp = cur.split("=");
+        cur = {
+            key:tmp[0],
+            value:tmp[1]
+        }
+
+        if(cur.key === "q"){
+            acc = cur.value
+        }
+        return acc;
+    }, undefined);
+
+    let google_link = "https://google.com/search?q="+q;
+    open(google_link);
+
+    console.log(search_arr);
+
+    window.close();
+}
 
 (function() {
     'use strict';
