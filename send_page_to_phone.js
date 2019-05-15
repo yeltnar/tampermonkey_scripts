cfc5d35337326c4c34e449588d6793bdc2616de5
yeltnar <andcbrant@gmail.com>
2019-05-15 10:57:33 -0500

    Create send_page_to_phone.js

---
+++ send_page_to_phone.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         send page to phone
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  send page to phone
+// @author       You
+// @match        http://*/*
+// @match        https://*/*
+// @match        chrome-extension://*/*
+// @grant        none
+// ==/UserScript==
+
+let last_key="";
+
+(()=>{
+    'use strict';
+    window.addEventListener("keyup",keyAction);
+    console.log("added send to join key listener");
+    function keyAction(e){
+        last_key += e.key;
+        if( /jjjjj/.test(last_key) ){
+           let url = window.location.href;
+           url = encodeURIComponent(url);
+           let req_url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?url=${url}&deviceId=60a30f961e6546798ba899bc6033c33f&apikey=4e5267df11734f0085829a771456ace9`;
+           fetch(req_url);
+        }
+    }
+})();
