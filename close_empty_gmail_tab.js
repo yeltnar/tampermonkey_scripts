181a44d93cd4003ea0cf45ee89c2788170da44b3
yeltnar <andcbrant@gmail.com>
2019-05-21 17:44:31 +0000

    Create close_empty_gmail_tab.js

---
+++ close_empty_gmail_tab.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Close empty gmail tab
+// @namespace    http://tampermonkey.net/
+// @version      0.0
+// @description  try to take over the world!
+// @author       You
+// @match        https://mail.google.com/mail/*
+// @grant        window.close
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("close empty gmail");
+    setTimeout(()=>{
+        if(document.querySelector("body").innerHTML===""){
+            window.close()
+        }
+    },5*1000);
+})();
