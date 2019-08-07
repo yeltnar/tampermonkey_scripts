be7699a5e5e50ae57f9b15cdb8502785467a55fd
yeltnar <andcbrant@gmail.com>
2019-08-07 20:08:44 +0000

    Update close_empty_gmail_tab.js

--- close_empty_gmail_tab.js
+++ close_empty_gmail_tab.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Close empty gmail tab
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://mail.google.com/mail/*
 // @match        https://www.google.com/url*
 // @match        https://www.google.com/*
+// @match        https://slack-redir.net/*
 // @grant        window.close
 // ==/UserScript==
 
