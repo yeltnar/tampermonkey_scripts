08598cfe119151df612423227a242c1cade8a71d
yeltnar <andcbrant@gmail.com>
2019-08-20 15:41:47 -0500

    Create FixVerse.js

---
+++ FixVerse.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Fix Verse
+// @namespace    http://tampermonkey.net/
+// @version      0.1.5
+// @description  reload verse if it is not populated
+// @author       You
+// @match        https://mail.notes.na.collabserv.com/*
+// @grant        none
+// ==/UserScript==
+
+(()=>{
+
+    let div=document.createElement("div");
+    div.innerText = "😀";
+    
+    if( document.querySelector("body").children.length === 0 ){
+        fixUrl();
+    }
+
+    if( document.querySelector(".ics-scbanner") ){
+        document.querySelector(".ics-scbanner").appendChild(div);
+    }
+
+   let bodyHtml = document.querySelector("body").innerHTML;
+   if(bodyHtml===" "||bodyHtml===""){
+      fixUrl();
+   }
+
+})/*()*/;
+
+(()=>{
+	const og_title = document.title;
+
+	setInterval(()=>{
+		document.title = `${og_title} ${new Date().toLocaleString('en-US',{timeStyle:'short'})}`
+	}, 1000);
+
+})();
+
+function fixUrl(){
+    window.location.href="https://mail.notes.na.collabserv.com/verse?";
+}
