d1dbb3ebb5678c70f8bbb4c83383ad1312dab6e1
yeltnar <andcbrant@gmail.com>
2020-02-13 00:02:54 -0600

    Update send_page_to_phone.js

--- send_page_to_phone.js
+++ send_page_to_phone.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         video settings
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://192.168.1.100:3333/*
