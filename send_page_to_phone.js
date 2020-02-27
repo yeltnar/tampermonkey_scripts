98ceed42a0c317ce533b6832c826e36adcb77f03
Drew Brantley <brantley@us.ibm.com>
2020-02-27 14:34:49 -0600

    send page to phone contents correction

--- send_page_to_phone.js
+++ send_page_to_phone.js
@@ -1,119 +1,81 @@
 // ==UserScript==
-// @name         video settings
+// @name         send page to phone
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
+// @version      0.2.4
+// @description  send page to phone
 // @author       You
-// @match        https://192.168.1.100:3333/*
+// @match        http://*/*
+// @match        https://*/*
+// @match        chrome-extension://*/*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
 
-(function() {
+let last_key="";
 
-    if( document.querySelectorAll("video").length===1 ){
-        try{
-            const remote_functions = getRemoteFunctions();
-            addVideoSettings(remote_functions);
-        }catch( e ){
-            console.error(e);
-            debugger
+(()=>{
+    'use strict';
+    window.addEventListener("keyup",keyAction);
+    console.log("added send to join key listener");
+    function keyAction(e){
+        last_key += e.key;
+        if( /jjjjj/.test(last_key) ){
+            sendThisPageToPhone()
         }
     }
 
-    async function addVideoSettings(remote_functions){
-
-        const saved_data = await remote_functions.getSavedData();
-
-        const video_element = document.querySelectorAll("video")[0];
-
-        video_element.style.maxWidth = "100%";
-        video_element.style.maxHeight = "100%";
-        video_element.style.width = "100%";
-        video_element.style.height = "100%";
-
-        const play_time = getItem("play_time");
-        if( play_time!==undefined ){
-            try{
-                console.log({play_time});
-                video_element.currentTime = parseInt(play_time);
-            }catch(e){
-                //throw e;
-            }
-        }
-
-        const paused = JSON.parse(getItem("paused"));
-        if( paused!==undefined ){
-            try{
-                console.log({paused});
-
-                video_element.pause();
-
-                if(paused===false){
-                    video_element.play();
-                }
-            }catch(e){
-                throw e;
-            }
-        }
-
-        setInterval((()=>{
-            const video_element = document.querySelectorAll("video")[0];
-            const {currentTime}=video_element;
-            const {paused} = video_element;
-            setItem("play_time",currentTime);
-            setItem("paused",paused);
-        }),1000);
-
-        setInterval((()=>{
-            console.log( getItem("play_time") );
-        }),10000);
-
-        console.log( getItem("play_time") );
-
-        console.log("video settings added");
-
+    function sendThisPageToPhone(){
+        let url = window.location.href;
+        url = encodeURIComponent(url);
+        sendToPhone(url);
 
     }
 
-    const prefex=(function (){
-        const arr=window.location.href.split("/");
-        return arr[arr.length-1];
-    })()
-
-    function getItem(str){
-        str = prefex+str;
-        return localStorage.getItem(str);
-    }
-
-    function setItem(str,val){
-        str = prefex+str;
-        return localStorage.setItem(str,val);
-    }
-
-    function getRemoteFunctions(){
-
-        function getSavedData(){
-            console.log("remote not yet ready");
+    function sendToPhone(url){
+        let apikey = GM_getValue('join_apikey');
+        if(apikey===undefined||apikey===null){
+            apikey = prompt("enter join api key");
+            GM_setValue('join_apikey', apikey);
         }
 
-        function saveData(){
-            console.log("remote not yet ready");
-        }
+        const deviceId = "group.android";
+        const title = document.title;
 
-        return {getSavedData,saveData};
+        let req_url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?url=${url}&title=${title}&deviceId=${deviceId}&apikey=${apikey}`;
+        console.log({req_url});
+        fetch(req_url);
+        last_key="";
+        console.log("called send to join");
+        notifyMe("called send to join");
     }
 
-    console.log(getProtectedValue("test"));
-
-    function getProtectedValue(name){
-        name = name; // optionally set prefix
-        let value = GM_getValue(name);
-        if(value===undefined||value===null){
-            value = prompt("enter "+name+" value");
-            GM_setValue(name, value);
-        }
-        return apikey;
-    }
+    window.sendToPhone = sendToPhone;
+    window.sendThisPageToPhone = sendThisPageToPhone;
 
 })();
+
+function notifyMe(msg) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support system notifications");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        var notification = new Notification(msg);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                var notification = new Notification("Hi there!");
+            }
+        });
+    }
+
+    // Finally, if the user has denied notifications and you
+    // want to be respectful there is no need to bother them any more.
+}
