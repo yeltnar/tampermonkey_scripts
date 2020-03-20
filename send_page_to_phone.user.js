d5d1fb3a49db015d9f6eaa79f68398aae2f57c3f
yeltnar <andcbrant@gmail.com>
2020-03-20 13:32:28 -0500

    Update send_page_to_phone.user.js

--- send_page_to_phone.user.js
+++ send_page_to_phone.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         send page to phone
 // @namespace    http://tampermonkey.net/
-// @version      0.2.5
+// @version      0.2.6
 // @description  send page to phone
 // @author       You
 // @match        http://*/*
@@ -80,7 +80,46 @@ function notifyMe(msg) {
             }
         });
     }
+  
+    showToast(3000)
 
     // Finally, if the user has denied notifications and you
     // want to be respectful there is no need to bother them any more.
+}
+
+function showToast(time){
+	const load = document.createElement("div");
+	load.setAttribute("findme", "drew");
+	load.style.position="absolute";
+	load.style.left="0px";
+	load.style.top="0px";
+	load.style.width="100%";
+	load.style.zIndex=Number.MAX_SAFE_INTEGER;
+  load.style.height="10px";
+
+  const start = new Date();
+
+  const load_time = 5*60*1000; // 5 min
+  const load_bar = document.createElement("div");
+  load_bar.style.width = "100%";
+  load_bar.style.backgroundColor="green";
+  load_bar.style.position="relitive";
+  load_bar.style.left="0px";
+  load_bar.style.top="0px";
+  
+  load_bar.innerHTML = "Sent to phone";
+  load_bar.style.color="white";
+  load_bar.style.textAlign="center";
+  load_bar.style.fontSize="2rem";
+  
+
+    load.appendChild(load_bar);
+
+	document.querySelector("body").appendChild(load);
+
+	console.log(document.querySelector("[findme=drew]"));
+
+	setTimeout(()=>{
+		document.querySelector("body").removeChild(load);
+	},time);
 }
