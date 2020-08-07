22ca711f912284213fff0a3f20fad2376df0de57
yeltnar <github@andbrant.com>
2020-08-07 14:21:30 -0500

    Update old-reddit.user.js

--- old-reddit.user.js
+++ old-reddit.user.js
@@ -4,15 +4,19 @@
 // @match       https://www.reddit.com/*
 // @match       https://www.reddit.com*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Andrew Brantley
 // @run-at      document-start
+// @grant       window.close
 // ==/UserScript==
 
 (()=>{
   
   console.log('before change')
   window.location.href = window.location.href.split("https://www.").join("https://old.");
+  setTimeout(()=>{
+    window.close();
+  },1000);
   console.log('after change')
   
 })();
