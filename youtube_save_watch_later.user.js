d5a0efd7b99cfd974388a2e29f8ff93cd5049738
yeltnar <github@andbrant.com>
2020-08-10 23:24:27 -0500

    Update youtube_save_watch_later.user.js

--- youtube_save_watch_later.user.js
+++ youtube_save_watch_later.user.js
@@ -3,16 +3,14 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/watch
 // @grant       none
-// @version     0.1.3
+// @version     0.1.4
 // @author      -
 // @run-at      document-idle
 // @description 3/23/2020, 3:18:31 PM
 // @require https://raw.githubusercontent.com/yeltnar/tampermonkey_scripts/master/q_term.notauser.js
 // @require https://raw.githubusercontent.com/yeltnar/tampermonkey_scripts/master/toast.notauser.js
-// 
+// @require https://raw.githubusercontent.com/yeltnar/tampermonkey_scripts/master/timeoutPromise.notauser.js
 // ==/UserScript==
-
-
 
 ;(async ()=>{
   console.log("adding youtube_save_watch_later");
@@ -42,33 +40,34 @@ function addKeyEvent(key_arr){
   });
 }
 
-async function saveWatchLater(){
+const saveWatchLater = (()=>{
   
-  toast("Saving to Watchlater", 5000);
+  const pageLoadedDelayPromise = timeoutPromise(2000);
   
-  console.log("starting youtube_save_watch_later");
-  
-  await timeoutPromise(2000);
+  async function saveWatchLater(){  
+    toast("Saving to Watchlater", 5000);
 
-  const watch_later_button = document.querySelector("ytd-button-renderer.ytd-menu-renderer:nth-child(4) > a:nth-child(1) > yt-icon-button:nth-child(1) > button:nth-child(1)");
-  watch_later_button.click();
+    console.log("starting youtube_save_watch_later");
+    
+    await pageLoadedDelayPromise;
+
+    const watch_later_button = document.querySelector("ytd-button-renderer.ytd-menu-renderer:nth-child(4) > a:nth-child(1) > yt-icon-button:nth-child(1) > button:nth-child(1)");
+    watch_later_button.click();
+
+    await timeoutPromise(1000);
+
+    const save_watch_later_check = document.querySelector('[aria-label="Watch later Private"]').parentElement.parentElement.parentElement.parentElement.querySelector("#checkboxContainer");
+    if( !save_watch_later_check.querySelector("#checkbox").classList.contains("checked") ){
+      save_watch_later_check.click();
+    }
+
+    await timeoutPromise(1000);
+
+    document.querySelector("yt-icon.ytd-add-to-playlist-renderer").click()
+  } 
   
-  await timeoutPromise(1000);
+  return saveWatchLater;  
+})();
 
-  const save_watch_later_check = document.querySelector('[aria-label="Watch later Private"]').parentElement.parentElement.parentElement.parentElement.querySelector("#checkboxContainer");
-  if( !save_watch_later_check.querySelector("#checkbox").classList.contains("checked") ){
-    save_watch_later_check.click();
-  }
 
-  await timeoutPromise(1000);
 
-  document.querySelector("yt-icon.ytd-add-to-playlist-renderer").click()
-}
-
-function timeoutPromise(ms){
-  
-  return new Promise((resolve, reject)=>{
-    setTimeout(resolve,ms);
-  });
-  
-}
