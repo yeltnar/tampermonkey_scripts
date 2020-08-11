4ebb3b551d19d1a379d107a357cf52b4ec2859de
yeltnar <github@andbrant.com>
2020-08-10 23:16:50 -0500

    Update send_page_to_phone.user.js

--- send_page_to_phone.user.js
+++ send_page_to_phone.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         send page to phone
 // @namespace    http://tampermonkey.net/
-// @version      0.2.13
+// @version      0.2.14
 // @description  send page to phone
 // @author       You
 // @match        http://*/*
@@ -9,6 +9,7 @@
 // @match        chrome-extension://*/*
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @require      https://raw.githubusercontent.com/yeltnar/tampermonkey_scripts/master/toast.notauser.js
 // ==/UserScript==
 
 let last_key="";
@@ -52,82 +53,13 @@ let last_key="";
         fetch(req_url);
         last_key="";
         console.log("called send to join");
-        notifyMe("called send to join");
+        toast("Sent to phone", 3000);
     }
 
     window.sendToPhone = sendToPhone;
     window.sendThisPageToPhone = sendThisPageToPhone;
 
 })();
-
-function notifyMe(msg) {
-    
-    showToast(3000);
-    return
-
-    // Let's check if the browser supports notifications
-    if (!("Notification" in window)) {
-        alert("This browser does not support system notifications");
-    }
-
-    // Let's check whether notification permissions have already been granted
-    else if (Notification.permission === "granted") {
-        // If it's okay let's create a notification
-        var notification = new Notification(msg);
-    }
-
-    // Otherwise, we need to ask the user for permission
-    else if (Notification.permission !== 'denied') {
-        Notification.requestPermission(function (permission) {
-            // If the user accepts, let's create a notification
-            if (permission === "granted") {
-                var notification = new Notification("Hi there!");
-            }
-        });
-    }
-  
-
-    // Finally, if the user has denied notifications and you
-    // want to be respectful there is no need to bother them any more.
-}
-
-function showToast(time){
-  
-	const load = document.createElement("div");
-	load.setAttribute("findme", "drew");
-	load.style.position="absolute";
-	load.style.left="0px";
-	load.style.top="0px";
-	load.style.width="100%";
-	load.style.zIndex=Number.MAX_SAFE_INTEGER;
-  load.style.height="10px";
-
-  const start = new Date();
-
-  const load_time = 5*60*1000; // 5 min
-  const load_bar = document.createElement("div");
-  load_bar.style.width = "100%";
-  load_bar.style.backgroundColor="green";
-  load_bar.style.position="fixed";
-  load_bar.style.left="0px";
-  load_bar.style.top="0px";
-  
-  load_bar.innerHTML = "Sent to phone";
-  load_bar.style.color="white";
-  load_bar.style.textAlign="center";
-  load_bar.style.fontSize="2rem";
-  
-
-  load.appendChild(load_bar);
-
-	document.querySelector("body").appendChild(load);
-
-	console.log(document.querySelector("[findme=drew]"));
-
-	setTimeout(()=>{
-		document.querySelector("body").removeChild(load);
-	},time);
-}
 
 function fixForYoutube(url){
   
