ee664854b5399acc25f5ac5071a12ce676538543
yeltnar <github@andbrant.com>
2020-08-10 22:53:23 -0500

    Create q_term.notauser.js

---
+++ q_term.notauser.js
@@ -0,0 +1,28 @@
+function getQValue(q_term="q"){
+
+    let search = window.location.search;
+
+    search = search.split("?")[1];
+
+    let search_arr = search.split("&");
+
+    let q = search_arr.reduce((acc, cur)=>{
+
+        if( acc!==undefined ){
+            return acc;
+        }
+
+        const tmp = cur.split("=");
+        cur = {
+            key:tmp[0],
+            value:tmp[1]
+        }
+
+        if(cur.key === q_term){
+            acc = cur.value
+        }
+        return acc;
+    }, undefined);
+
+    return q;
+}
