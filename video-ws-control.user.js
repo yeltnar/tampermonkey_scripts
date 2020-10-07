965ebc93b5740c67c75a5a3ec6a180609c0e5925
yeltnar <github@andbrant.com>
2020-10-07 14:34:37 -0500

    working nice?

--- video-ws-control.user.js
+++ video-ws-control.user.js
@@ -1,17 +1,35 @@
 // ==UserScript==
-// @name        video ws control
+// @name        video ws controll
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/watch*
 // @grant       none
-// @version     1.0
+// @version     0.1
 // @author      -
 // @description 10/7/2020, 11:58:24 AM
+// @run-at      document-end
 // ==/UserScript==
+
+const ACTIONS = {
+  play:"play",
+};
+
+const video_element = document.querySelector("video");
+
 
 (()=>{
     const url = "wss://Node-WSS.yeltnar.repl.co";
 
     let socket = new WebSocket(url);
+  
+    // add event listeners for the video
+    video_element.addEventListener("play",()=>{
+      console.log('you clicked play');
+      socket.send(JSON.stringify(getPlayAction()));
+    });
+    video_element.addEventListener("pause",()=>{
+      console.log('you clicked pause');
+      socket.send(JSON.stringify(getPauseAction()));
+    });
   
     socket.onopen = function(e) {
         const obj = {
@@ -42,22 +60,53 @@
       alert(`[error] ${error.message}`);
     };
   
+    setTimeout(()=>{
+      console.log('sending pause')
+      socket.send(JSON.stringify(getPauseAction()));
+      setTimeout(()=>{
+        console.log('sending play')
+        socket.send(JSON.stringify(getPlayAction()));
+      },5000)
+      
+    },5000)
+  
   })();
-
-
-function a(){
-  console.log('another thing')
-}
 
 
 function parseAction(action_obj){
   console.log(`parseAction`)
-  const video_element = document.querySelector("video");
   
   console.log(`parseAction - ${JSON.stringify(action_obj, null, 2)}`);
   
+  if( action_obj.url !== window.location.href ){
+      console.log(JSON.stringify({
+        action_url: action_obj.url,
+        curent_url: window.location.href
+      },null,2))
+      return;
+  }
+  
   if( action_obj.action==="pause" ){
     video_element.pause();
-    console.log('pausing')
-  } 
+    console.log('pausing');
+  }else if( action_obj.action==="play" ){
+    video_element.play();
+    console.log('playing')
+  }
+}
+
+function getPlayAction(){
+  return {
+    "videocontrol":true,
+    "action":"play",
+    url: window.location.href,
+  }
+}
+
+function getPauseAction(){
+  return {
+    "videocontrol":true,
+    "action":"pause",
+    url: window.location.href,
+  }
 }
