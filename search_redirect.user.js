c8424c2c63b0147e509286c2eb6462c4bc4e79c9
yeltnar <github@andbrant.com>
2021-01-07 10:20:57 -0600

    Update search_redirect.user.js

--- search_redirect.user.js
+++ search_redirect.user.js
@@ -2,8 +2,8 @@
 // @name        search redirect
 // @namespace   Violentmonkey Scripts
 // @match       https://www.startpage.com/do/dsearch
-// @grant       none
-// @version     0.1
+// @grant       window.close
+// @version     0.2
 // @author      -
 // @description 1/7/2021, 9:52:00 AM
 // @run-at document-start
@@ -16,7 +16,17 @@
   console.log(`query is ${query}`);
   
   if( /fi sms ?/.test(query) ){
-      window.location.href = `https://accounts.google.com/AccountChooser/signinchooser?continue=https%3A%2F%2Fmessages.google.com%2Fweb%2Fu%2F0%2FpostSignIn&flowName=GlifWebSignIn&flowEntry=AccountChooser`;
+    
+    const new_url = `https://accounts.google.com/AccountChooser/signinchooser?continue=https%3A%2F%2Fmessages.google.com%2Fweb%2Fu%2F0%2FpostSignIn&flowName=GlifWebSignIn&flowEntry=AccountChooser`;
+    window.location.href = new_url;
+    
+    const interval = setInterval(()=>{
+      if(new_url!==window.location.href){
+        clearInterval(interval);
+        window.close();
+      }
+    },100);
+    
   }
   
 })();
