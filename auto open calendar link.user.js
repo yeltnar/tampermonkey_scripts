2549e69f6db40d93dfd7bb4fe8dded172af72184
Drew Brantley <brantley@us.ibm.com>
2021-03-17 11:24:41 -0500

    fix issue with different timezones

--- auto open calendar link.user.js
+++ auto open calendar link.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://mail.notes.na.collabserv.com/livemail/($Calendar)/*
 // @grant       none
-// @version     0.1
+// @version     0.2
 // @author      -
 // @run-at      document-idle
 // @description 3/15/2021, 9:55:34 AM
@@ -21,9 +21,12 @@
     const online_meeting = textEleSearch("Online Meeting"); // prefer this one?
     const location = textEleSearch("Location");
     console.log({online_meeting,location});
+    
+    const ele = online_meeting===undefined?location:online_meeting;
+    console.log({location_ele:ele});
 
     // `https://` all the way to the end 
-    const link_ele = getCousinEle({tester:/(https?:\/\/.*)/, ele:online_meeting===undefined?location:online_meeting});
+    const link_ele = getCousinEle({tester:/(https?:\/\/.*)/, ele});
     const link = link_ele.innerText.split(" ")[0];
     //.split(" ")[0];
     console.log({
@@ -33,8 +36,9 @@
     
     
     // meeting_time = 
-    const time_ele = textEleSearch("Time:");
-    const time_cousin_ele = getCousinEle({tester:/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/,ele:time_ele});
+    const time_ele = textEleSearch(/time:/i);
+    let time_cousin_ele = getCousinEle({tester:/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/,ele:time_ele});
+    time_cousin_ele = Array.isArray(time_cousin_ele) ? time_cousin_ele[0] : time_cousin_ele;
     const time_str = time_cousin_ele.innerText.split(" - ")[0];
     console.log({
       time_cousin_ele,
@@ -44,7 +48,7 @@
     
     const open_in = new Date(time_str).getTime()-new Date().getTime() -45*1000 ; // some buffer before meeting actually starts 
     
-    console.log({open_in});
+    console.log({open_in,link});
     
     if( open_in > -1*1000*60*10 ){ // if too far in the past, don't open
       setTimeout(()=>{
