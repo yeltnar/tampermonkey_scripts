48dc0c91249207e8ef96991ef0ffd8323baf780a
Drew Brantley <brantley@us.ibm.com>
2021-04-02 10:04:41 -0500

    update xmlhttprequest

--- XMLHttpRequestPromise.notauser.js
+++ XMLHttpRequestPromise.notauser.js
@@ -1,26 +1,13 @@
-function old_XMLHttpRequestPromise(url,method="GET"){
-  return new Promise((resolve,reject)=>{
-    var xhr = GM_xmlhttpRequest();
-    xhr.withCredentials = true;
-    xhr.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        resolve(this.responseText);
-      }
-    });
-    xhr.open(method, url);
-    xhr.send();
-  });
-}
-
-
 function xmlHttpRequest(url, method = "GET") {
-
   return new Promise((resolve,reject)=>{
-    GM.xmlHttpRequest({
+    GM_xmlHttpRequest({
       method,
       url,
       onload: function (response) {
         resolve(response);
+      },
+      onerror: function(err){
+        reject(err);
       }
     });
   });
