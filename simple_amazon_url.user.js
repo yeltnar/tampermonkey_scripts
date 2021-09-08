4ee8da287a07220945ca7f3de9bbf1ac77c8e653
yeltnar <github@andbrant.com>
2021-09-08 12:28:21 -0500

    Update simple_amazon_url.user.js

--- simple_amazon_url.user.js
+++ simple_amazon_url.user.js
@@ -3,19 +3,43 @@
 // @namespace   andbrant
 // @match       https://www.amazon.com/*
 // @grant       GM_registerMenuCommand
-// @version     0.2
-// @author      -
+// @version     0.3
+// @author      github/yeltnar
 // @dont-require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/timeoutPromise.notauser.js
 // @dont-require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/textEleSearch.notauser.js
+// @run-at  document-start
 // ==/UserScript==
 
 // TODO need no autoload or smart autoload
 
 function simpleAmazonURL(){  
   let product = /\/(([A-Z]|[0-9]){10})/.exec(window.location.href)[1];
-  window.location.href = `https://amazon.com/dp/${product}`;
+  
+  if( window.location.href !== `https://www.amazon.com/dp/${product}` ){
+    console.log({
+      msg:"moving to simple page",
+      href: window.location.href,
+      new: `https:/www./amazon.com/dp/${product}`
+    });
+    window.location.href = `https://amazon.com/dp/${product}`;
+  }
 }
 
 (()=>{
   GM_registerMenuCommand("Simple Amazon URL", simpleAmazonURL);
 })();
+
+(()=>{
+
+  if( localStorage.getItem('simple-amazon-redirect')===null ){
+    localStorage.setItem('simple-amazon-redirect','true');
+  }
+  const v = localStorage.getItem('simple-amazon-redirect');
+  
+  console.log({v});
+  
+  if(v==='true'){
+    simpleAmazonURL();
+  }
+
+})()
