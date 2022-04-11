583cd1a2cf63e94bbb10baf31dadd667a0339d27
Drew Brantley <brantley@us.ibm.com>
2022-04-11 16:43:58 -0500

    fix api key bug

--- expressFirebase.notauser.js
+++ expressFirebase.notauser.js
@@ -10,7 +10,7 @@ const expressFirebase = (()=>{
 
   async function sendToJoin({url, title="default title", deviceId="group.android", text}){
     const apikey = getJoinApiKey();
-    let req_url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?title=${title}&deviceId=${deviceId}`
+    let req_url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?apikey=${apikey}&title=${title}&deviceId=${deviceId}`
     req_url += text!==undefined ? `&text=${text}` : "";
     req_url += url!==undefined ? `&url=${url}` : "";
     console.log({ req_url });
