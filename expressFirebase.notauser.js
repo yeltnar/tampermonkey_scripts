6e766be63450b7e76b7fb06f5de5bae8e6da9398
Drew Brantley <brantley@us.ibm.com>
2022-04-11 12:51:27 -0500

    text for join push

--- expressFirebase.notauser.js
+++ expressFirebase.notauser.js
@@ -2,16 +2,17 @@ const expressFirebase = (()=>{
 
   async function sendToPhone(url) {
 
-
     const deviceId = "group.android";
     const title = document.title;
 
     return await sendToJoin({url, title, deviceId});
   }
 
-  async function sendToJoin({url, title="default title", deviceId="group.android"}){
+  async function sendToJoin({url, title="default title", deviceId="group.android", text}){
     const apikey = getJoinApiKey();
-    let req_url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?url=${url}&title=${title}&deviceId=${deviceId}&apikey=${apikey}`;
+    let req_url = `https://joinjoaomgcd.appspot.com/_ah/api/messaging/v1/sendPush?title=${title}&deviceId=${deviceId}`
+    req_url += text!==undefined ? `&text=${text}` : "";
+    req_url += url!==undefined ? `&url=${url}` : "";
     console.log({ req_url });
     const reply = await GM_xmlHttpRequestPromise(req_url);
     console.log("called send to join");
