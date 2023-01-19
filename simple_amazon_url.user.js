94ae9802657dc59698d21224a22e1a2264fdb796
yeltnar <github@andbrant.com>
2023-01-19 09:56:39 -0600

    don't redirect amazon shop links

--- simple_amazon_url.user.js
+++ simple_amazon_url.user.js
@@ -3,7 +3,7 @@
 // @namespace   andbrant
 // @match       https://www.amazon.com/*
 // @grant       GM_registerMenuCommand
-// @version     0.5
+// @version     0.6
 // @author      github/yeltnar
 // @dont-require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/timeoutPromise.notauser.js
 // @dont-require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/textEleSearch.notauser.js
@@ -12,24 +12,29 @@
 
 // TODO need no autoload or smart autoload
 
-function simpleAmazonURL(){  
+function simpleAmazonURL(){
   let product = /\/(([A-Z]|[0-9]){10})/.exec(window.location.href)[1];
-  
+
   // don't redirect wishlists
   if( window.location.href.includes('wishlist') ){
     return
   }
-  
+
+  // don't redirect shops
+  if( window.location.href.includes('amazon.com/shop') ){
+    return
+  }
+
   // don't redirect reviews
   if( window.location.href.includes('product-reviews') ){
     return
   }
-  
+
   // don't redirect simple urls
   if( window.location.href === `https://www.amazon.com/dp/${product}` ){
     return
   }
-  
+
   console.log({
     msg:"moving to simple page",
     href: window.location.href,
@@ -48,9 +53,9 @@ function simpleAmazonURL(){
     localStorage.setItem('simple-amazon-redirect','true');
   }
   const v = localStorage.getItem('simple-amazon-redirect');
-  
+
   console.log({v});
-  
+
   if(v==='true'){
     simpleAmazonURL();
   }
