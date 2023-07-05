5affe2503597f0d0c7c351456751ceb44f3f51f2
Drew Brantley <brantley@us.ibm.com>
2023-07-05 09:19:05 -0500

    add zillow search

--- search_redirect.user.js
+++ search_redirect.user.js
@@ -19,7 +19,7 @@
 // @grant       GM_removeValueChangeListener
 // @grant       GM_setValue
 // @grant       GM_notification
-// @version     0.51
+// @version     0.52
 // @author      yeltnar
 // @description 1/7/2021, 9:52:00 AM
 // @require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/timeoutPromise.notauser.js
@@ -181,6 +181,12 @@ function main(query){
       generic: true,
       regex_res_index:2,
       base_str:"https://www.homedepot.com/s/"
+    },
+    {
+      regex:/^(zillow) (.*)/,
+      generic: true,
+      regex_res_index:2,
+      base_str:`https://www.zillow.com/homes/`
     },
     {
       regex:/()(.*)/,
