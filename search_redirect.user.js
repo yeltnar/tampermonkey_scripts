6e640a9225a69a509ff4720352188b01995f5aa5
Andrew Brantley <github@andbrant.com>
2024-03-17 16:53:49 -0500

    different nixos search

--- search_redirect.user.js
+++ search_redirect.user.js
@@ -19,7 +19,7 @@
 // @grant       GM_removeValueChangeListener
 // @grant       GM_setValue
 // @grant       GM_notification
-// @version     0.62
+// @version     0.63
 // @author      yeltnar
 // @description 1/7/2021, 9:52:00 AM
 // @require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/timeoutPromise.notauser.js
@@ -203,7 +203,7 @@ function main(query){
       base_str:`https://who.is/whois/`
     },
     {
-      regex:/^(nixos) (.*)/,
+      regex:/^(\?nixos) (.*)/,
       generic: true,
       regex_res_index:2,
       base_str:`https://search.nixos.org/packages?query=`
