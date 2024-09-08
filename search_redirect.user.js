7f7cfbe2a0700a964734b93e832373bc9c1b6588
Drew Brantley <github@andbrant.com>
2024-09-08 13:44:00 -0500

    add protondb search

--- search_redirect.user.js
+++ search_redirect.user.js
@@ -19,7 +19,7 @@
 // @grant       GM_removeValueChangeListener
 // @grant       GM_setValue
 // @grant       GM_notification
-// @version     0.72
+// @version     0.73
 // @author      yeltnar
 // @description 1/7/2021, 9:52:00 AM
 // @require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/timeoutPromise.notauser.js
@@ -235,6 +235,12 @@ function main(query){
       generic: true,
       regex_res_index:2,
       base_str: `https://gprivate.com/search/?q=`
+    },
+    {
+      regex: /(protondb) (.*)/,
+      generic: true,
+      regex_res_index:2,
+      base_str: `https://www.protondb.com/search?q=`
     },
     {
       regex:/()(.*)/,
