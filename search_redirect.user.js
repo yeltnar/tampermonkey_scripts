2e874c37fb19e76957b4cc9cb8ee843119e9b4f2
drew <github@andbrant.com>
2026-08-10 09:06:12 -0500

    add searxng redirect

--- search_redirect.user.js
+++ search_redirect.user.js
@@ -19,7 +19,7 @@
 // @grant       GM_removeValueChangeListener
 // @grant       GM_setValue
 // @grant       GM_notification
-// @version     0.86
+// @version     0.87
 // @author      yeltnar
 // @description 1/7/2021, 9:52:00 AM
 // @require     https://github.com/yeltnar/tampermonkey_scripts/raw/master/timeoutPromise.notauser.js
@@ -299,6 +299,12 @@ function main(query){
       generic: true,
       regex_res_index:2,
       base_str: `https://chatgpt.com/?prompt=`
+    },
+    {
+      regex: /^(searxng|sng|sxng) (.*)/,
+      generic: true,
+      regex_res_index:2,
+      base_str: `https://searxng.h.lan/search?q=`
     },
     {
       regex:/()(.*)/,
